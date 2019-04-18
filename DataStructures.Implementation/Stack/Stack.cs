d49e51341af8144bf85d50bb65129ace997316cc
Armen <armena1998@yahoo.com>
2019-04-18 20:25:10 +0400

    Initial create of Stack algorithm using DoubleLinkedList.

---
+++ DataStructures.Implementation/Stack/Stack.cs
@@ -0,0 +1,52 @@
+﻿using DataStructures.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Stack
+{
+    public class Stack<T>
+        where T : struct
+    {
+        private DoubleLinkedList<T> list;
+        public Stack()
+        {
+            list = new DoubleLinkedList<T>();
+        }
+        public int Count { get { return list.Count; } }
+
+        public void Push(T item)
+        {
+            list.AddLast(item);
+        }
+
+        public T Pop()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Stack is empty");
+
+            var last = list.Last;
+            list.RemoveLast();
+
+            return last.Value;
+        }
+
+        public T Peek()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Stack is empty");
+
+            return list.Last.Value;
+        }
+
+        public bool Contains(T item)
+        {
+            return list.Contains(item);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+    }
+}
