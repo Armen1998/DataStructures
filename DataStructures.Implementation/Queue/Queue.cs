25729b5c554adc913bc1a72de31e9af328b06f7e
Armen <armena1998@yahoo.com>
2019-04-19 16:28:39 +0400

    Initial commit of Queue using DoubleLinkedList.

---
+++ DataStructures.Implementation/Queue/Queue.cs
@@ -0,0 +1,53 @@
+﻿using DataStructures.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Queue
+{
+    public class Queue<T> 
+        where T : struct
+    {
+        private DoubleLinkedList<T> list;
+        public Queue()
+        {
+            list = new DoubleLinkedList<T>();
+        }
+
+        public int Count { get { return list.Count; } }
+
+        public void Enqueue(T item)
+        {
+            list.AddLast(item);
+        }
+
+        public T Dequeue()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Queue is empty");
+
+            var first = list.First.Value;
+            list.RemoveFirst();
+
+            return first;
+        }
+
+        public T Peek()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Queue is empty");
+
+            return list.First.Value;
+        }
+
+        public bool Contains(T item)
+        {
+            return list.Contains(item);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+    }
+}
