5875e67de5b1f9586389851fd50dee567b5a9e2e
Armen <armena1998@yahoo.com>
2019-05-04 17:16:14 +0400

    Finishe main functionality of HashSet using Hashtable.

--- DataStructures.Implementation/Sets/HashSet.cs
+++ DataStructures.Implementation/Sets/HashSet.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace DataStructures.Sets
 {
-    public class HashSet<T>
-        where T : struct
+    public class HashSet<T> : ICollection<T>, IEnumerable<T>, IEnumerable
     {
         private Hashtable hashTable;
         public HashSet()
@@ -19,18 +19,20 @@ namespace DataStructures.Sets
         {
             foreach (var item in collection)
             {
-                if (!hashTable.ContainsValue(item))
-                    hashTable.Add(Hash(item.ToString()), item);
+                Add(item);
             }
         }
+
+        public int Count => hashTable.Count;
+
+        public bool IsReadOnly => hashTable.IsReadOnly;
 
         private string Hash(string item)
         {
             using (SHA256 sha256Hash = SHA256.Create())
             {
                 byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(item));
-
-                // Convert byte array to a string   
+                
                 StringBuilder builder = new StringBuilder();
                 for (int i = 0; i < bytes.Length; i++)
                 {
@@ -40,17 +42,72 @@ namespace DataStructures.Sets
             }
         }
 
-        public int Count => hashTable.Count;
-
-        public bool IsReadOnly => hashTable.IsReadOnly;
-
         public void Add(T item)
         {
             if (!hashTable.ContainsValue(item))
                 hashTable.Add(Hash(item.ToString()), item);
         }
 
-        public bool IsSupersetOf(ICollection<T> other)
+        public void IntersectWith(IEnumerable<T> other)
+        {
+            if (other is null)
+                throw new ArgumentNullException();
+
+            var list = new List<T>();
+
+            foreach(var item in hashTable.Values)
+            {              
+                if (!other.Contains((T)item))
+                {
+                    list.Add((T)item);
+                }
+            }
+
+            list.ForEach(item => Remove(item));
+        }
+
+        public int RemoveWhere(Func<T, bool> predicate)
+        {
+            var array = new T[hashTable.Count];
+            hashTable.Values.CopyTo(array, 0);
+
+            var list = array.Where(predicate).ToList();
+
+            list.ForEach(item => Remove(item));
+
+            return list.Count;
+        }
+
+        public void ExceptWith(IEnumerable<T> other)
+        {
+            if (other is null)
+                throw new ArgumentNullException();
+
+            var list = new List<T>();
+
+            foreach (var item in hashTable.Values)
+            {
+                if (other.Contains((T)item))
+                {
+                    list.Add((T)item);
+                }
+            }
+
+            list.ForEach(item => Remove(item));
+        }
+
+        public void UnionWith(IEnumerable<T> other)
+        {
+            if (other is null)
+                throw new ArgumentNullException();
+
+            foreach (var item in other)
+            {
+                Add(item);
+            }
+        }
+
+        public bool IsSupersetOf(IEnumerable<T> other)
         {
             if (other is null)
                 throw new ArgumentNullException();
@@ -64,7 +121,7 @@ namespace DataStructures.Sets
             return true;
         }
 
-        public bool IsSubsetOf(ICollection<T> other)
+        public bool IsSubsetOf(IEnumerable<T> other)
         {
             if (other is null)
                 throw new ArgumentNullException();
@@ -86,11 +143,36 @@ namespace DataStructures.Sets
         public bool Contains(T item)
         {
             return hashTable.ContainsValue(item);
+        }       
+
+        public void CopyTo(T[] array, int arrayIndex = 0)
+        {  
+            hashTable.Values.CopyTo(array, arrayIndex);
         }
-        
-        public void Remove(T item)
+    
+        public bool Remove(T item)
         {
-            hashTable.Remove(Hash(item.ToString()));
+            if(hashTable.ContainsValue(item))
+            {
+                var b = hashTable.ContainsKey(Hash(item.ToString()));
+                hashTable.Remove(Hash(item.ToString()));
+                return true;
+            }
+
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in hashTable.Values)
+            {
+                yield return (T)item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
