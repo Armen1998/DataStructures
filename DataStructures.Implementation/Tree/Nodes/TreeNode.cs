04c7f64fac3ab9f8b5760cae04e89752a8d04c74
Armen <armena1998@yahoo.com>
2019-05-04 21:11:33 +0400

    Initial commit of TreeNode.

---
+++ DataStructures.Implementation/Tree/Nodes/TreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Tree.Nodes
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; set; }
+        public List<TreeNode<T>> Childs { get; set; }
+
+        public TreeNode()
+        {
+            
+        }
+
+        public TreeNode(T value) : this()
+        {
+            Value = value;           
+        }
+    }
+}
