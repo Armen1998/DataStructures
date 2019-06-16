dd5207fa535b63667763ddc13a001c24700422cd
Armen <armena1998@yahoo.com>
2019-06-16 15:06:52 +0400

    Initial commit.

---
+++ DataStructures.Implementation/Tree/Nodes/BinaryTreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Implementation.Tree.Nodes
+{
+    public class BinaryTreeNode<T>
+    {
+        public T Value { get; set; }
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Right { get; set; }
+
+        public BinaryTreeNode() { }     
+
+        public BinaryTreeNode(T value)
+        {
+            Value = value;
+        }
+
+        public void Display() => Console.Write(Value + " ");     
+    }
+}
