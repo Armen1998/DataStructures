9abc5c085b193132f5d96405834e8dfb65e6ae9b
Armen <armena1998@yahoo.com>
2019-06-16 15:09:22 +0400

    Add test for Tree and BinarySearchTree.

--- DataStructures.Test/Program.cs
+++ DataStructures.Test/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataStructures.Implementation.Tree;
+using DataStructures.Tree;
+using System;
 using System.Collections.Generic;
 
 namespace DataStructures.Test
@@ -71,17 +73,75 @@ namespace DataStructures.Test
             #endregion
 
             #region HashSet
-            Sets.HashSet<int> hashSet = new Sets.HashSet<int>(new List<int>() { 1, 2, 3, 8, 9, 15, 7, 1 });
-            Sets.HashSet<int> hashSet2 = new Sets.HashSet<int> { 1, 2, 3, 8, 9, 10, 6, 1 };
-            Console.WriteLine(hashSet.Contains(10));
-            hashSet.UnionWith(hashSet2);
-            hashSet.ExceptWith(new List<int>() { 8, 9 });            
-            hashSet.IntersectWith(new List<int> { 3, 8, 9, 10, 6, 1 });
-            Console.WriteLine(hashSet2.IsSupersetOf(new List<int> { 10, 6, 1 }));
-            Console.WriteLine(hashSet2.IsSubsetOf(new List<int> { 1, 2, 3, 8, 9, 10, 6, 11, 12 }));
-            hashSet.Remove(9);
-            Console.WriteLine(hashSet.RemoveWhere(item => item > 5));
+            //Sets.HashSet<int> hashSet = new Sets.HashSet<int>(new List<int>() { 1, 2, 3, 8, 9, 15, 7, 1 });
+            //Sets.HashSet<int> hashSet2 = new Sets.HashSet<int> { 1, 2, 3, 8, 9, 10, 6, 1 };
+            //Console.WriteLine(hashSet.Contains(10));
+            //hashSet.UnionWith(hashSet2);
+            //hashSet.ExceptWith(new List<int>() { 8, 9 });            
+            //hashSet.IntersectWith(new List<int> { 3, 8, 9, 10, 6, 1 });
+            //Console.WriteLine(hashSet2.IsSupersetOf(new List<int> { 10, 6, 1 }));
+            //Console.WriteLine(hashSet2.IsSubsetOf(new List<int> { 1, 2, 3, 8, 9, 10, 6, 11, 12 }));
+            //hashSet.Remove(9);
+            //Console.WriteLine(hashSet.RemoveWhere(item => item > 5));
             #endregion
+
+            #region Tree
+            //Tree<int> tree = new Tree<int>(5);
+            //Console.WriteLine(tree.IsEmpty());
+            //Console.WriteLine(tree.Find(tree.Root, 15) is null);
+            //tree.Add(tree.Root, 10);
+            //tree.Add(tree.Root, 20);
+            //tree.Add(tree.Root, 30);
+            //Console.WriteLine(tree.Find(tree.Root, 30) is null);
+            //tree.Add(tree.Find(tree.Root, 20), 21);
+            //tree.Add(tree.Find(tree.Root, 20), 22);
+            //tree.Add(tree.Find(tree.Root, 20), 23);
+            //tree.Add(tree.Find(tree.Root, 30), 31);
+            //tree.Add(tree.Find(tree.Root, 30), 32);
+            //tree.Add(tree.Find(tree.Root, 30), 33);
+            //Console.WriteLine(tree.Find(tree.Root, 22) is null);
+            #endregion
+
+            #region BinarySearchTree
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.Insert(10);
+            binarySearchTree.Insert(15);
+            binarySearchTree.Insert(7);
+            binarySearchTree.Insert(23);
+            binarySearchTree.Insert(12);
+            binarySearchTree.Insert(8);
+            binarySearchTree.Insert(5);
+            binarySearchTree.Insert(21);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(1);
+            binarySearchTree.Insert(11);
+            binarySearchTree.Insert(13);
+            binarySearchTree.Insert(3);
+            binarySearchTree.Insert(4);
+            binarySearchTree.Insert(2);
+            binarySearchTree.Insert(-1);
+            binarySearchTree.Insert(20);
+            binarySearchTree.Insert(19);
+            binarySearchTree.InOrder(binarySearchTree.Root);
+            Console.WriteLine();
+            binarySearchTree.InOrderIterative(binarySearchTree.Root);
+            Console.WriteLine();
+            binarySearchTree.PreOrder(binarySearchTree.Root);
+            Console.WriteLine();
+            binarySearchTree.PreOrderIterative(binarySearchTree.Root);
+            Console.WriteLine();
+            binarySearchTree.PostOrder(binarySearchTree.Root);
+            Console.WriteLine();
+            binarySearchTree.PostOrderIterative(binarySearchTree.Root);
+            Console.WriteLine();
+            Console.WriteLine(binarySearchTree.Height(binarySearchTree.Find(7)));
+
+            ////Console.WriteLine(binarySearchTree.FindMin(binarySearchTree.Root));            
+            ////Console.WriteLine(binarySearchTree.FindMax(binarySearchTree.Root));
+            ////Console.WriteLine(binarySearchTree.Find(21).Value);
+            ////Console.WriteLine(binarySearchTree.Delete(4));
+            #endregion
+
             Console.ReadKey();
         }
     }
